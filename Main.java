8f6bd61f436a6f5ceb7d6afee1b415404d1f18ce
Damenpa <damenpa@gmail.com>
2026-05-13 00:05:02 -0600

    Quite archivos raretes

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("file.encoding", "UTF-8");
+        
         // Iniciar la aplicación GUI en el Event Dispatch Thread
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
